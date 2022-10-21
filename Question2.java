250047674c04b43dd16159460ecfcb61172978bc
nareshbhukya007 <116295301+nareshbhukya007@users.noreply.github.com>
2022-10-21 18:53:12 +0530

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,15 @@
+package com.ineuron.assingment1;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+
+		int n = 5;
+		for (int i = 1; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+}
